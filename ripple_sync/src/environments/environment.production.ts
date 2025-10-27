ab722aeade0888bdcec81382edc9d668d5b88634
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-10-27 09:39:25 +0100

    Fixed backend base URL

--- ripple_sync/src/environments/environment.production.ts
+++ ripple_sync/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ripplesync-backend.graybeach-8775421e.northeurope.azurecontainerapps.io',
+  apiUrl: 'https://ripplesync-backend.graybeach-8775421e.northeurope.azurecontainerapps.io/api',
 };
