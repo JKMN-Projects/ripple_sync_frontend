9e9f49ce7f59e2445401a85a3016f90d15cc3dd5
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-10-28 14:02:03 +0100

    Added styling overrides and worked on home page

--- ripple_sync/src/app/pages/home/home.ts
+++ ripple_sync/src/app/pages/home/home.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { Stack } from '../../components/stack/stack';
+import { MatCardModule } from '@angular/material/card';
+import { IconContainer } from "../../components/icon-container/icon-container";
 
 @Component({
   selector: 'app-home',
-  imports: [MatButton, MatGridListModule, Stack],
+  imports: [
+    MatButton,
+    MatGridListModule,
+    MatCardModule,
+    Stack,
+    IconContainer
+],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
