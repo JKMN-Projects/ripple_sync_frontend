700cacde692fa35cc5183d897e6def63d40e6dce
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-11-03 11:09:34 +0100

    Added data fetching to dashboard page

---
+++ ripple_sync/src/app/services/dashboard-service.ts
@@ -0,0 +1,37 @@
+import { httpResource } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+interface DashboardTotal {
+  publishedPosts: number;
+  scheduledPosts: number;
+  totalReach: number;
+  totalLikes: number;
+  statsForPlatforms: StatsForPlatform[];
+}
+
+interface StatsForPlatform{
+  platform: string;
+  publishedPosts: number;
+  reach: number;
+  likes: number;
+  engagement: number;
+  averageEngagement: number;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private _dashboardData = httpResource<DashboardTotal>(() => `${environment.apiUrl}/dashboard/total`, {
+    // parse: (res: DashboardTotal) => res
+  });
+  get dashboardData() {
+    return this._dashboardData.asReadonly();
+  }
+
+  reloadDashboardData() {
+    this._dashboardData.reload();
+  }
+}
