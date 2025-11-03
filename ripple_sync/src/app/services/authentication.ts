40348f8fb4612d89c6e63c64d23d856fd3809c66
NJ-DELFI <84667145+SirJul1337@users.noreply.github.com>
2025-11-03 12:04:54 +0100

    Delete button now works

--- ripple_sync/src/app/services/authentication.ts
+++ ripple_sync/src/app/services/authentication.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponseBase } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { map, Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -26,83 +26,128 @@ export interface AuthenticationResponse {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Authentication {
   router = inject(Router);
 
   isAuthenticated = signal<boolean>(false);
-  userEmail = signal<string>("");
-  registerState = signal<RegisterResponseState>({ status: null, message: null, validationErrors: null });
+  userEmail = signal<string>('');
+  registerState = signal<RegisterResponseState>({
+    status: null,
+    message: null,
+    validationErrors: null,
+  });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(credentials: LoginRequest): Observable<AuthenticationResponse> {
-    return this.http.post<AuthenticationResponse>(
-      `${environment.apiUrl}/authentication/login`,
-      credentials,
-      { observe: 'response' }
-    ).pipe(
-      tap({
-        next: response => {
-          if (response.ok) {
-            this.userEmail.set(response.body?.email ?? '');
-            this.isAuthenticated.set(true);
-            localStorage.setItem("expiresAt", response.body?.expiresAt.toString() ?? "");
-            localStorage.setItem("email", response.body?.email.toString() ?? "");
-          }
-        },
-        error: () => {
-          this.isAuthenticated.set(false);
-        }
-      }),
-      map(response => response.body as AuthenticationResponse)
-    );
+    return this.http
+      .post<AuthenticationResponse>(`${environment.apiUrl}/authentication/login`, credentials, {
+        observe: 'response',
+      })
+      .pipe(
+        tap({
+          next: (response) => {
+            if (response.ok) {
+              this.userEmail.set(response.body?.email ?? '');
+              this.isAuthenticated.set(true);
+              localStorage.setItem('expiresAt', response.body?.expiresAt.toString() ?? '');
+              localStorage.setItem('email', response.body?.email.toString() ?? '');
+            }
+          },
+          error: () => {
+            this.isAuthenticated.set(false);
+          },
+        }),
+        map((response) => response.body as AuthenticationResponse)
+      );
   }
 
   register(register: RegisterRequest): void {
     this.registerState.set({ status: 'loading', message: null, validationErrors: null });
-    this.http.post<ProblemDetails>(
-      `${environment.apiUrl}/authentication/register`,
-      register,
-      { observe: 'response' }
-    ).pipe(
-      tap({
-        next: response => {
-          if (response.ok) {
-            this.registerState.update(state => ({ ...state, status: 'success', message: null, validationErrors: null }) );
-          }
-        },
-        error: (error) => {
-          const problemDetails = parseProblemDetails(error.error);
-          if (error.status === 400 && problemDetails?.validationErrors) {
-            this.registerState.update(state => ({ ...state, status: 'error', message: 'Some fields have validation errors', validationErrors: problemDetails.validationErrors || null }));
-          }
-          else if (error.status >= 400 && error.status < 500 && problemDetails) {
-            this.registerState.update(state => ({ ...state, status: 'error', message: 'An error occurred. ' + problemDetails.detail }));
-          }
-          else {
-            this.registerState.update(state => ({ ...state, status: 'error', message: 'An unexpected error occurred. Please try again later.' }));
-          }
-        }
+    this.http
+      .post<ProblemDetails>(`${environment.apiUrl}/authentication/register`, register, {
+        observe: 'response',
       })
-    ).subscribe();
+      .pipe(
+        tap({
+          next: (response) => {
+            if (response.ok) {
+              this.registerState.update((state) => ({
+                ...state,
+                status: 'success',
+                message: null,
+                validationErrors: null,
+              }));
+            }
+          },
+          error: (error) => {
+            const problemDetails = parseProblemDetails(error.error);
+            if (error.status === 400 && problemDetails?.validationErrors) {
+              this.registerState.update((state) => ({
+                ...state,
+                status: 'error',
+                message: 'Some fields have validation errors',
+                validationErrors: problemDetails.validationErrors || null,
+              }));
+            } else if (error.status >= 400 && error.status < 500 && problemDetails) {
+              this.registerState.update((state) => ({
+                ...state,
+                status: 'error',
+                message: 'An error occurred. ' + problemDetails.detail,
+              }));
+            } else {
+              this.registerState.update((state) => ({
+                ...state,
+                status: 'error',
+                message: 'An unexpected error occurred. Please try again later.',
+              }));
+            }
+          },
+        })
+      )
+      .subscribe();
   }
   checkExpiresAt() {
-    const expiresAt = Number.parseInt(localStorage.getItem("expiresAt") ?? "0");
+    const expiresAt = Number.parseInt(localStorage.getItem('expiresAt') ?? '0');
 
     if (new Date().getTime() < expiresAt) {
       this.isAuthenticated.set(true);
-      this.userEmail.set(localStorage.getItem("email") ?? "");
-    }
-    else {
+      this.userEmail.set(localStorage.getItem('email') ?? '');
+    } else {
       // Call refresh token endpoint and move this.logout to that method.
       this.logout();
     }
   }
-
+  private removeLocalStorage(): void {
+    localStorage.removeItem('expiresAt');
+    localStorage.removeItem('email');
+  }
   logout(): void {
     this.isAuthenticated.set(false);
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
+    this.removeLocalStorage();
+  }
+  deleteAccount(): void {
+    this.registerState.set({ status: 'loading', message: null, validationErrors: null });
+    this.http
+      .delete<HttpResponseBase>(`${environment.apiUrl}/authentication/user`, {
+        observe: 'response',
+      })
+      .pipe(
+        tap({
+          next: (response) => {
+            if (response.status == 204) {
+              this.removeLocalStorage();
+              this.logout();
+            }
+          },
+          error: (error) => {
+            alert("Error happended while trying to delete user try again later")
+          },
+        })
+      )
+      .subscribe();
   }
 }
